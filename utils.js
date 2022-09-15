dcb62bae9bc094ca1caace59a1aa9ee0fa808c61
Jason Fu <jason.fz@hotmail.com>
2022-09-15 14:51:27 -0700

    add subtruct  function

--- utils.js
+++ utils.js
@@ -2,4 +2,6 @@ function product(num1,num2) {return num1 * num2}
 
 function divide(num1,num2) {return num1 / num2}
 
-function divide(num1,num2) {return num1 ** num2}
+function power(num1,num2) {return num1 ** num2}
+
+function subtract(num1,num2) {return num1 - num2}
